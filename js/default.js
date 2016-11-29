0ac0692f6383a07c71030c8a631038c712dd8774
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2016-11-29 16:55:55 +0530

    update

--- js/default.js
+++ js/default.js
@@ -14,7 +14,6 @@
 
 //default: digital clock default format: (HH:mm:ss)
 var clockObj = new dyClock($("#dyclock-digital"));
-console.log(clockObj);
 clockObj.start();
 
 //default: digital clock default format: (HH:mm:ss) border
@@ -25,7 +24,6 @@ var clockObj2 = new dyClock($("#dyclock-digital-2"), {
         border : '1px solid #999'
     }
 });
-console.log(clockObj2);
 clockObj2.start();
 
 //default: digital clock default format: (HH:mm:ss) and digitalStyle
@@ -40,7 +38,6 @@ var clockObj3 = new dyClock($("#dyclock-digital-3"), {
         fontFamily : 'Faster One'
     }
 });
-console.log(clockObj3);
 clockObj3.start();
 
 //default: digital clock format: (HH:mm:ss A)
@@ -48,7 +45,6 @@ var clockObj4 = new dyClock($(".dyclock-digital-4"), {
     clock : "digital",
     format : "hh:mm:ss A"
 });
-console.log(clockObj4);
 clockObj4.start();
 
 //default: digital clock format: (HH:mm a)
@@ -56,14 +52,12 @@ var clockObj5 = new dyClock($(".dyclock-digital-5"), {
     clock : "digital",
     format : "hh:mm a"
 });
-console.log(clockObj5);
 clockObj5.start();
 
 //analog clock
 var clockObj6 = new dyClock($("#dyclock-analog-6"), {
     clock : "analog"
 });
-console.log(clockObj6);
 clockObj6.start();
 
 //analog clock - image 1
@@ -71,7 +65,6 @@ var clockObj7 = new dyClock($("#dyclock-analog-7"), {
     clock : "analog",
     image : "image/c01.png"
 });
-console.log(clockObj7);
 clockObj7.start();
 
 //analog clock - image 2
@@ -86,7 +79,6 @@ var clockObj8 = new dyClock($("#dyclock-analog-8"), {
         fontSize : 28,
     }
 });
-console.log(clockObj8);
 clockObj8.start();
 
 //analog clock - image 3
@@ -121,5 +113,4 @@ var clockObj9 = new dyClock($(".dyclock-analog-9"), {
         shape : "circle"
     }
 });
-console.log(clockObj9);
 clockObj9.start();
